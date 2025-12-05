2447de915c4433b4d83287202e3f6f04c2d86d46
Kawser420 <muhammadkawser202@gmail.com>
2025-12-06 00:22:38 +0600

    feat: not found route finished

--- src/server.ts
+++ src/server.ts
@@ -221,6 +221,15 @@ app.get("/todos", async (req: Request, res: Response) => {
   }
 });
 
+// Not Found
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
