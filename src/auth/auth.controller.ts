3f10da2eefe0c5ccd05b065bbd4d47ff4b236b8e
Kawser420 <muhammadkawser202@gmail.com>
2025-12-09 18:37:21 +0600

    auth controller comment remove

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,6 @@ const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
     const result = await authServices.loginUser(email, password);
-    // console.log(result.rows[0]);
     res.status(200).json({
       success: false,
       message: "Login Successful",
